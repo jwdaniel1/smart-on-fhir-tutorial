7cecaff7900d0f2dfc789e809926aa3c44a9061a
jwdaniel1 <52797760+jwdaniel1@users.noreply.github.com>
2026-04-29 14:32:12 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -63,6 +63,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.bodyTemp = getQuantityValueAndUnit(bodyTemp[0]);
 
           ret.resolve(p);
         });
@@ -87,6 +88,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      bodyTemp: {value: ''},
     };
   }
 
